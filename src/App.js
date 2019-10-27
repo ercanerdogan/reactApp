f1ab1d9b479959cf63bcdab6e1d25675171974c4
Ercan-home <ercanerdogan@gmail.com>
2019-10-28 00:14:35 +0300

    usage of reactstrap

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import Navi from "./Navi";
 import CategoryList from "./CategoryList";
 import ProductList from './ProductList';
+import { Container, Row, Col} from "reactstrap"
 
 function App() {
   return (
     <div>
-      <Navi/>
-      <CategoryList/>
-      <ProductList/>
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList />
+          </Col>
+          <Col xs="9">
+            <ProductList />
+
+           
+          </Col>
+
+
+
+        </Row>
+      </Container>
+
     </div>
   );
 }
