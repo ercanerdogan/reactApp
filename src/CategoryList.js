7a12689feb2170baed9c034f955655833da186d2
Ercan-home <ercanerdogan@gmail.com>
2019-10-31 07:49:07 +0300

    working with events

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react'
 
-import {ListGroup, ListGroupItem} from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 
 export default class CategoryList extends Component {
     //it's not necessary in newest version of react
     // constructor(props){
     //     super(props)
-        
+
     // }
-    
-    state={
-        categories : [
-            { categoryID : 1, categoryName : "Beverages"},
-            { categoryID : 2, categoryName : "Condiments"}
-        ]
+
+    state = {
+        categories: [
+            { categoryID: 1, categoryName: "Beverages" },
+            { categoryID: 2, categoryName: "Condiments" }
+        ],
+        currentCategory: ""
 
     };
+
+    getCurrentCategory = (category) => {
+        this.setState({ currentCategory: category.categoryName })
+    };
+
     render() {
         return (
             <div>
                 <h3>{this.props.info.title}</h3>
                 <ListGroup>
-                    {this.state.categories.map(category=>(
-                     <ListGroupItem key={category.categoryID}>{category.categoryName}</ListGroupItem>   
+                    {this.state.categories.map(category => (
+                        <ListGroupItem
+                            onClick={() => this.getCurrentCategory(category)}
+                            key={category.categoryID}>
+                            {category.categoryName}
+                        </ListGroupItem>
                     ))}
-                    
+
                 </ListGroup>
+                <h4>{this.state.currentCategory}</h4>
             </div>
         )
     }
